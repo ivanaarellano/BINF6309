57b576a02c8c2a70bfc0839e401766d678095b9c
arellano.i <arellano.i@husky.neu.edu>
2019-09-15 23:24:12 -0400

    Find unique k-mers

--- BioPython/bp_kmers.py
+++ BioPython/bp_kmers.py
@@ -15,18 +15,16 @@ for record in SeqIO.parse('/scratch/Drosophila/dmel-all-chromosome-r6.17.fasta',
         Sequence = str(Sequence) # Makes sequence a string
         TempFile.write(Sequence) # writes the sequence into temp file
 
-
-
-
-
-
-
-
-
-
-
-
-
+TempFile.close() # closes temp file
+KmersList = [] # Makes a list for all possible kmers
+kmer_counts = dict() # makes empty dictionary
+uniquekmer= ""  # makes a variable with all the kmers
+dna_string = open("TempFile.txt", "r") # opnes TempFile for reading
+PosKmer = itertools.product("ACTG", repeat=6) #iter tools to make all possible kmers with length of 6
+for i in PosKmer:
+    Joined = ''.join(i) # joins i with all the possible kmers
+    KmersList.append(Joined) # appends list with next possible kmer
+#print(KmersList) #prints kmers list
 
 if dna_string.mode == "r": # if file is open for reading
     contents = dna_string.read() # reads the file and names the contents in file contents
