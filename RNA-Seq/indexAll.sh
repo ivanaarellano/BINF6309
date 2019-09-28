c063a4e17e4dadfa3fbf404932f06a3ffc14ff74
arellano.i <arellano.i@husky.neu.edu>
2019-09-28 11:34:43 -0400

    indexAll.sh

--- RNA-Seq/indexAll.sh
+++ RNA-Seq/indexAll.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-# indexAll.sh
-#samtools index \
-#Aip02.sorted.bam \
-#1>Aip02.index.log 2>Aip02.index.err &
-
-#!/usr/bin/env bash
-# SortAll.sh 
+# indexAll.sh 
 #Initialize variable to contain the directory of bam bai files 
 BamPath="/home/arellano.i/BINF6309/RNA-Seq/bam/"
 #Initialize variable to contain the suffix bam files
@@ -16,15 +10,13 @@ for left in $BamPath*$BamSuffix
 do
     #Remove the path from the filename and assign to pathRemoved
     pathRemoved="${left/$BamPath/}"
-    echo $pathRemoved
+    #echo $pathRemoved
     #Remove the Sam suffix from $pathRemoved and assign to suffixRemoved
     sampleName="${pathRemoved/$BamSuffix/}"
-    echo $sampleName
-    echo samtools index
-    echo $BamPath$sampleName$BamSuffix
-    echo $BamPath$sampleName.index.log 
-    echo $ErrOutPath$sampleName.index.err
-   #1>$BamPath$sampleName.index.log 2>$ErrOutPath$sampleName.index.err &
+    #echo $sampleName
+    samtools index \
+    $BamPath$sampleName$BamSuffix \
+    1>$BamPath$sampleName.index.log 2>$ErrOutPath$sampleName.index.err &
 done
 
 
