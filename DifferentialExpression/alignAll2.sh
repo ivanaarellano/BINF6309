bf646f88b4706f86389a137a44a3f51fc6e3668e
arellano.i <arellano.i@husky.neu.edu>
2019-09-30 18:56:16 -0400

    RMarkdown

--- DifferentialExpression/alignAll2.sh
+++ DifferentialExpression/alignAll2.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # AlignAll.sh 
 #Initialize variable to contain the directory of un-trimmed fastq files 
-PairedPath="/scratch/SampleDataFiles/Paired"
+PairedPath="/scratch/SampleDataFiles/Paired/"
 #Initialize variable to contain the suffix for the left reads
 outDir='quant/'
-sample='Aip02'
-leftSuffix=".R1.fastq"
-rightSuffix=".R2.fastq"
+#sample='Aip02'
+leftSuffix=".R1.paired.fastq"
+rightSuffix=".R2.paired.fastq"
 ErrOutPath="/home/arellano.i/BINF6309/DifferentialExpression/Err"
 #Loop through all the left-read fastq files in $fastqPath
 for leftInPaired in $PairedPath*$leftSuffix
 do
     #Remove the path from the filename and assign to pathRemoved
-    pathRemoved="${/scratch/SampleDataFiles/Paired}"
+    pathRemoved="${leftInPaired/$PairedPath/}"
     #echo $pathRemoved 
     #Remove the left-read suffix from $pathRemoved and assign to suffixRemoved
     sampleName="${pathRemoved/$leftSuffix/}"
@@ -21,26 +21,26 @@ do
 
 
 
-    $PairedPath$sampleName$leftSuffix \
-    $PairedPath$sampleName$rightSuffix \
-    1>$SamOutPath$sampleName.sam 2>$ErrOutPath$sampleName.err
+    #$PairedPath$sampleName$leftSuffix \
+    #$PairedPath$sampleName$rightSuffix \
+    #1>$SamOutPath$sampleName.sam 2>$ErrOutPath$sampleName.err
 done
 
 
 
 
 #!/usr/bin/env bash
-outDir='quant/'
-sample='Aip02'
-function align {
-    salmon quant -l IU is \
-        -1 /scratch/SampleDataFiles/Paired/$sample.R1.paired.fastq \
-        -2 /scratch/SampleDataFiles/Paired/$sample.R2.paired.fastq \
-        -i AipIndex \
-        --validateMappings \
-        -o $outDir$sample
-}
+#outDir='quant/'
+#sample='Aip02'
+#function align {
+#    salmon quant -l IU is \
+#        -1 /scratch/SampleDataFiles/Paired/$sample.R1.paired.fastq \
+#        -2 /scratch/SampleDataFiles/Paired/$sample.R2.paired.fastq \
+#        -i AipIndex \
+#        --validateMappings \
+#        -o $outDir$sample
+#}
 
-align 1>align.log 2>align.err &
+#align 1>align.log 2>align.err &
 
 
