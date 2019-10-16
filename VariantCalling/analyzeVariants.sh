0a629726a58499570b2d74740f1e5ed327b02432
arellano.i <arellano.i@husky.neu.edu>
2019-10-15 20:11:07 -0400

    Variant

--- VariantCalling/analyzeVariants.sh
+++ VariantCalling/analyzeVariants.sh
@@ -4,8 +4,8 @@ sudo docker run \
   gcr.io/deepvariant-docker/deepvariant:"${BIN_VERSION}" \
   /opt/deepvariant/bin/run_deepvariant \
   --model_type=WGS \ **Replace this string with exactly one of the following [WGS,WES,PACBIO]**
-  --ref=/input/ucsc.hg19.chr20.unittest.fasta \
-  --reads=/input/NA12878_S1.chr20.10_10p1mb.bam \
+  --ref=/input/GRCh38.primary_assembly.genome.fa.gz \
+  --reads=/input/GRCh38.bam \
   --regions "chr20:10,000,000-10,010,000" \
   --output_vcf=/output/output.vcf.gz \
   --output_gvcf=/output/output.g.vcf.gz \
