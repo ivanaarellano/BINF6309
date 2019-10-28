f3ef3bf5985a3de0422e627244ade41ab0c6f257
ivanaarellano <arellano.i@husky.neu.edu>
2019-10-28 19:28:56 -0400

    atacama

---
+++ 16S/Atacama.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+#Atacama Tutorial 
+
+mkdir qiime2-atacama-tutorial
+cd qiime2-atacama-tutorial
+
+wget \
+  -O "sample-metadata.tsv" \
+  "https://data.qiime2.org/2019.7/tutorials/atacama-soils/sample_metadata.tsv"
+
+mkdir emp-paired-end-sequences
+
+wget \
+  -O "emp-paired-end-sequences/forward.fastq.gz" \
+  "https://data.qiime2.org/2019.7/tutorials/atacama-soils/10p/forward.fastq.gz"
+
+wget \
+  -O "emp-paired-end-sequences/reverse.fastq.gz" \
+  "https://data.qiime2.org/2019.7/tutorials/atacama-soils/10p/reverse.fastq.gz"
+
+wget \
+  -O "emp-paired-end-sequences/barcodes.fastq.gz" \
+  "https://data.qiime2.org/2019.7/tutorials/atacama-soils/10p/barcodes.fastq.gz"
+
+qiime tools import \
+   --type EMPPairedEndSequences \
+   --input-path emp-paired-end-sequences \
+   --output-path emp-paired-end-sequences.qza
+
+qiime demux emp-paired \
+  --m-barcodes-file sample-metadata.tsv \
+  --m-barcodes-column barcode-sequence \
+  --p-rev-comp-mapping-barcodes \
+  --i-seqs emp-paired-end-sequences.qza \
+  --o-per-sample-sequences demux.qza \
+  --o-error-correction-details demux-details.qza
+
+qiime demux summarize \
+  --i-data demux.qza \
+  --o-visualization demux.qzv
+
+qiime dada2 denoise-paired \
+  --i-demultiplexed-seqs demux.qza \
+  --p-trim-left-f 13 \
+  --p-trim-left-r 13 \
+  --p-trunc-len-f 150 \
+  --p-trunc-len-r 150 \
+  --o-table table.qza \
+  --o-representative-sequences rep-seqs.qza \
+  --o-denoising-stats denoising-stats.qza
+
+qiime feature-table summarize \
+  --i-table table.qza \
+  --o-visualization table.qzv \
+  --m-sample-metadata-file sample-metadata.tsv
+
+qiime feature-table tabulate-seqs \
+  --i-data rep-seqs.qza \
+  --o-visualization rep-seqs.qzv
+
+qiime metadata tabulate \
+  --m-input-file denoising-stats.qza \
+  --o-visualization denoising-stats.qzv
+
+
+
