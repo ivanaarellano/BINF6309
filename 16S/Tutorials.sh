3d65e2b9aeca75070633a0cd1b98c003d4397aa3
arellano.i <arellano.i@husky.neu.edu>
2019-10-29 22:06:11 -0400

    Tutorials Rendered4

--- 16S/Tutorials.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#moving-pictures-Rmd.sh and atacama.sh
-R -e "rmarkdown::render('Tutorials_Render.Rmd', output_format='md_document')"
-
---
+++ 16S/methods16s.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#moving-pictures-Rmd.sh and atacama.sh
+R -e "rmarkdown::render('methods16s.Rmd', output_format='md_document')"
+
