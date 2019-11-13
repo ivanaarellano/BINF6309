eb487c81476ca5392f2c55d31ed7f6b73a68ca7f
ivanaarellano <arellano.i@husky.neu.edu>
2019-11-12 20:39:42 -0500

    lung data

--- Metagenomics/lungDataRender.sh
+++ Metagenomics/lungDataRender.sh
@@ -1,6 +1,6 @@
 
 #!/usr/bin/env bash
 #lungDataRender.sh and atacama.sh
-R -e "rmarkdown::render('lunData.Rmd', output_format='md_document')"
+R -e "rmarkdown::render('lungData.Rmd', output_format='md_document')"
 
 
